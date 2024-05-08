9e009b0ae878eb1ae0432a16db0186d4b7c16e58
Sevenwings26 <iarowosola@yahoo.com>
2024-05-08 22:47:21 +0100

    Update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -19,3 +19,9 @@ function cancel(){
 };
 
 
+// add margin to divs 
+const detailedR = document.getElementById('style-cont1');
+const fullyC = document.getElementById('style-cont2');
+
+detailedR.style.marginTop = "50px";
+fullyC.style.marginTop = "100px";
