43809cd895cfa4c08f97d0aad3192e2fcd61b0a7
Sevenwings26 <iarowosola@yahoo.com>
2024-05-09 15:06:53 +0100

    Responsiveness completed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -25,3 +25,35 @@ const fullyC = document.getElementById('style-cont2');
 
 detailedR.style.marginTop = "50px";
 fullyC.style.marginTop = "100px";
+
+
+
+// Sign-up and Login in 
+// get modal id
+const modal = document.getElementById('register');
+const modalLogin = document.getElementById('login-id');
+
+const closeModal = document.getElementById('close-signup');
+let closeModalLogin = document.getElementById('close-login');
+
+
+function signup(){
+    if (modal.style.display = 'none'){
+        modal.style.display = 'block';
+    }
+}
+
+function login(){
+    if (modalLogin.style.display = 'none'){
+        modalLogin.style.display = 'block';
+    }
+};
+
+closeModal.addEventListener('click', (e) => {
+    modal.style.display = 'none';
+});
+
+closeModalLogin.addEventListener('click', (e) => {
+    modalLogin.style.display = 'none';
+});
+
